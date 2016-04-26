de5529253c228b540b16b525515aae14d7a45935
Peter Flynn <fredoflynn@gmail.com>
2016-04-25 17:41:07 -0700

    initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+//refactor to function based view
+var Nav = React.createClass({
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">
+              React Time App
+            </li>
+            <li>
+              <Link to="/">Timer</Link>
+            </li>
+            <li>
+              <Link to="#">Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">Created by <a href="http://www.peterflynn.net">Peter Flynn</a></li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Nav;
